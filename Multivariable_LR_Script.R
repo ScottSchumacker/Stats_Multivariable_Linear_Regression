d7d19a6d043b478d2f652a804f26a02c4e194e4a
Scott Schumacker <scottschu97@gmail.com>
2025-02-22 08:55:17 -0500

    Added code to create data set

--- Multivariable_LR_Script.R
+++ Multivariable_LR_Script.R
@@ -4,3 +4,25 @@
 # Loading packages
 library(tidyverse)
 library(lmtest)
+
+# Creating data set
+n <- 200
+
+mileage <- rnorm(n, mean = 50000, sd = 20000)
+year <- round(rnorm(n, mean = 2015, sd = 5))
+engine_size <- runif(n, min = 1.5, max = 4.0)
+horsepower <- round(rnorm(n, mean = 200, sd = 50))
+mileage <- pmax(mileage, 1000)
+horsepower <- pmax(horsepower, 70)
+
+true_coefficients <- c(1000, -0.1, 1500, 200, 50)
+error_sd <- 3000
+
+price <- true_coefficients[1] +
+  true_coefficients[2] * mileage +
+  true_coefficients[3] * (year - 2000) +
+  true_coefficients[4] * engine_size +
+  true_coefficients[5] * horsepower +
+  rnorm(n, 0, error_sd)
+
+car_data <- data.frame(price, mileage, year, engine_size, horsepower)
